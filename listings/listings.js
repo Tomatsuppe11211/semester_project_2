74354d273e81297a65de94ab738eeac404a0fea2
Tomatsuppe11211 <marcos.lindboe47@gmail.com>
2026-03-17 01:44:52 +0100

    fetching listings and small fixes on design

--- listings/listings.js
+++ listings/listings.js
@@ -3,6 +3,17 @@ const loggedInMobileNav = document.getElementById('loggedOutMobileNav');
 const loggedOutMobileNav = document.getElementById('loggedInMobileNav');
 
 
+async function getListings(){
+    const response = await fetch('https://v2.api.noroff.dev/auction/listings')
+    const data = await response.json()
+    const listings = data.data
+    for(let i = 0; i < listings.length; i++){
+        console.log(listings[i].title)
+    }
+}
+
+getListings()
+
 
 
 /*Creating functions for opening and closing navigationbars for mobile*/
