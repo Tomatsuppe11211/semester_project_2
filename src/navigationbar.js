eef1d2b8e197938fa7d53b10f2cf2bee75c3e663
Tomatsuppe11211 <142995184+Tomatsuppe11211@users.noreply.github.com>
2026-03-23 19:37:12 +0100

    Header update
    - rewrote the header so both mobile nav and desktop nav only has one nav-tag each.
    
    - made the logos redirect the user back to the landing page
    
    - resized and changed the active link in the mobile nav.

--- src/navigationbar.js
+++ src/navigationbar.js
@@ -1,9 +1,23 @@
 //Getting mobile menu
 const mobileNav = document.getElementById('mobileNav');
 
+//Getting logos
+const headerLogo = document.getElementById('headerLogo');
+const footerLogo = document.getElementById('footerLogo');
+
+function logoSend(){
+    window.location.href = '../index.html'
+}
+
+headerLogo.addEventListener('click', logoSend);
+footerLogo.addEventListener('click', logoSend);
+
+
 //Getting toggler buttons for mobileNav
 const menuToggler = document.getElementById('menuIcon')
 const closeNavbarButton = document.getElementById('closeBurgerMenu');
+
+
 
 //Adding a close and open function for the mobile nav
 function closemobileNav(){
@@ -11,11 +25,9 @@ function closemobileNav(){
 }
 
 function openMobileNav(){
-    mobileNav.classList = 'flex md:hidden fixed flex-col w-50 bg-white border-l top-0 right-0 px-10 h-screen gap-5'
+    mobileNav.classList = 'flex md:hidden fixed flex-col w-fit px-10 bg-white border-l top-0 right-0 px-2 h-screen gap-5'
 }
 
 //Adding functions to buttons
 closeNavbarButton.addEventListener('click', closemobileNav);
 menuToggler.addEventListener('click', openMobileNav);
-
-
